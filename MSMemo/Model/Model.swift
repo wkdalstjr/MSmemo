7f1e543f5027da4b3c5db6e93b26f2264d3c1656
wkdalstjr <wkdalstjr1218@naver.com>
2021-06-16 17:54:50 +0900

    dataManager // db구현

--- MSMemo/Model/Model.swift
+++ MSMemo/Model/Model.swift
@@ -8,17 +8,17 @@
 
 import Foundation
 
-class Memo {
-    var content: String
-    var insertDate: Date
-    
-    init(content: String) {
-        self.content = content
-        insertDate = Date()
-    }
-    
-    static var dummyMemoList = [
-        Memo(content: "Lorem Ipsum"),
-        Memo(content: "Subscribe + 👍 = ❤️")
-    ]
-}
+//class Memo {
+//    var content: String
+//    var insertDate: Date
+//
+//    init(content: String) {
+//        self.content = content
+//        insertDate = Date()
+//    }
+//
+//    static var dummyMemoList = [
+//        Memo(content: "Lorem Ipsum"),
+//        Memo(content: "Subscribe + 👍 = ❤️")
+//    ]
+//}
