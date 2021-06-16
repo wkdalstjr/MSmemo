b79570c82db63299d83b30a3ae08db2bde65be12
wkdalstjr <wkdalstjr1218@naver.com>
2021-06-16 14:04:32 +0900

    Initial Commit

---
+++ MSMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MSMemo
+//
+//  Created by 장민석 on 2021/06/16.
+//  Copyright © 2021 장민석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
