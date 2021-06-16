d664a89752d683314d3c43d7d39aff40ca45d615
wkdalstjr <wkdalstjr1218@naver.com>
2021-06-16 14:51:49 +0900

    메인 화면 메모 저장

---
+++ MSMemo/Model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  MSMemo
+//
+//  Created by 장민석 on 2021/06/16.
+//  Copyright © 2021 장민석. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Lorem Ipsum"),
+        Memo(content: "Subscribe + 👍 = ❤️")
+    ]
+}
