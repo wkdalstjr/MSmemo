37bf157a04d5fc55f4d0dbe70885be9f6de53fde
wkdalstjr <wkdalstjr1218@naver.com>
2021-06-17 19:37:21 +0900

    메모를 데이터베이스에 저장 후 메인화면에 표시

--- MSMemo/Data/DataManager.swift
+++ MSMemo/Data/DataManager.swift
@@ -36,6 +36,17 @@ class DataManager {
         
     }
     
+    func addNewMemo(_ memo: String?) {
+        let newMemo = Memo(context: mainContent)
+        newMemo.content = memo
+        newMemo.insertDate = Date()
+        
+        memoList.insert(newMemo, at: 0)
+        
+        saveContext()
+    }
+    
+    
     
     // MARK: - Core Data stack
 
